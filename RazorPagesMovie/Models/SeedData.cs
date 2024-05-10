008fb1b38e85bb669d25312916f57a48fb64c190
Jina W <jina.wilde@my.utsa.edu>
2024-05-10 12:55:46 -0500

    added ratings field

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -1,52 +1,64 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RazorPagesMovie;
+﻿#define Rating
+#if Rating
+#region snippet_1 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using System;
+using System.Linq;
 
-namespace RazorPagesMovie.Models;
-
-public static class SeedData
+namespace RazorPagesMovie.Models
 {
-    public static void Initialize(IServiceProvider serviceProvider)
-    {
-        using (var context = new RazorPagesMovieContext(
-            serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+    public static class SeedData
+{
+        public static void Initialize(IServiceProvider serviceProvider)
         {
-            if (context == null || context.Movie == null)
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
             {
-                throw new ArgumentNullException("Null RazorPagesMovieContext");
-            }
-
-            // Look for any movies.
-            if (context.Movie.Any()) 
-            { 
-                return; // DB has been seeded
-            }
-
-            context.Movie.AddRange(
-                new Movie
+                if (context == null || context.Movie == null)
                 {
-                    Title = "Making Cookies",
-                    ReleaseDate = DateTime.Parse("2023-2-23"),
-                    Genre = "Nonfiction",
-                    Price = 7.99M
-                },
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
 
-                new Movie
+                // Look for any movies.
+                if (context.Movie.Any())
                 {
-                    Title = "Learning to Read",
-                    ReleaseDate = DateTime.Parse("2024-4-13"),
-                    Genre = "Nonfiction",
-                    Price = 9.99M
-                },
+                    return; // DB has been seeded
+                }
 
-                new Movie
-                {
-                    Title = "Going Swimming",
-                    ReleaseDate = DateTime.Parse("2003-1-8"),
-                    Genre = "Nonfiction",
-                    Price = 3.99M
-                });
-            context.SaveChanges();
+                #region snippet1
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Making Cookies",
+                        ReleaseDate = DateTime.Parse("2023-2-23"),
+                        Genre = "Nonfiction",
+                        Price = 7.99M,
+                        Rating = "G"
+                    },
+                #endregion
+                    new Movie
+                    {
+                        Title = "Learning to Read",
+                        ReleaseDate = DateTime.Parse("2024-4-13"),
+                        Genre = "Nonfiction",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Going Swimming",
+                        ReleaseDate = DateTime.Parse("2003-1-8"),
+                        Genre = "Nonfiction",
+                        Price = 3.99M,
+                        Rating = "G"
+                    });
+                context.SaveChanges();
             }
+        }
     }
  }
+#endregion
+#endif
